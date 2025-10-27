8855467776aa495252c3c2f691d567f8c9a9a598
vanyaWEB <dimid0403@gmail.com>
2025-10-27 10:35:37 +0500

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime, ForeignKey, Text, Index
+from sqlalchemy import Column, Integer, BigInteger, String, Float, Boolean, DateTime, ForeignKey, Text, Index
 from sqlalchemy.orm import relationship, declarative_base
 from datetime import datetime
 
@@ -9,7 +9,7 @@ class User(Base):
     __tablename__ = 'users'
     
     id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer, unique=True, nullable=False, index=True)
+    telegram_id = Column(BigInteger, unique=True, nullable=False, index=True)
     username = Column(String(255))
     first_name = Column(String(255))
     last_name = Column(String(255))
